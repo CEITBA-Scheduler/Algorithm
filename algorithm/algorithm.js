b6ced306950af506f64a58c72cc84d975ba950d4
Lucas Kammann <lucaskammann@hotmail.com>
2019-12-30 18:32:31 -0300

    searchCombination() added

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -1,14 +1,73 @@
+// Priority Types
+const PriorityType = {
+    NONE: "none",
+    COMMISSION: "commission",
+    PROFESSOR: "professor",
+    LOCATION: "location",
+    BUSYTIME: "busytime",
+    FREEDAY: "freeday",
+    SUPERPOSITION: "superposition"
+}
+
+
 /**
  * Searchs all possible combinations of schedules based on the given
  * subjects and their possible time tables.
  * @param   {Subject Object}        subjects    User's subjects
  * @param   {Callable}              verifier    Priority and Criteria verifier callback
- * @param   {Array of Combination}  combinations     
+ * @param   {Combination}           combination     
  * @returns {Array of Combination}      
  */
-var searchCombinations = function(subjects, verifier, combinations)
+var searchCombinations = function(subjects, verifier, combination = undefined)
 {
-    // TODO!
+    // When the function is first called, no combination has been generated, so
+    // I need to create a default instance of this object
+    if (combination === undefined)
+    {
+        combination = {
+            weight: NaN,
+            priorities: [],
+            subjects: []
+        };
+    }
+
+    // Checking if we still have more subjects to continue with the tree of
+    // possible combinations
+    if (subjects.length > 0)
+    {
+        // Pops a subject from the list of subjects and iterates over all possible
+        // commissions creating for each a new possible combination on the tree
+        var combinations = [];
+        let nodeSubject = subjects.pop();
+        let nodeCommissions = nodeSubject.commissions;
+
+        for (let nodeCommission of nodeCommissions)
+        {
+            var newCombination = JSON.parse(JSON.stringify(combination));
+            newCombination.subjects.push(
+                {
+                    name: nodeSubject.name,
+                    commissionName: nodeCommission.commissionName,
+                    teachers: nodeCommission.teachers,
+                    commissionTimes: nodeCommission.commissionTimes
+                }
+            );
+
+            // Get the new possible combinations for each commission case and concatenates them
+            // with the current result of combinations
+            var recursiveCombinations = searchCombinations(subjects.slice(), verifier, newCombination);
+            combinations = combinations.concat(recursiveCombinations);
+        }
+
+        // Returns the possible combinations
+        return combinations;
+    }
+    else
+    {
+        // When there are not subjects left, we have to check if the current combination created
+        // is valid according to the criteria or priorities taken by the algorithm user...
+        return verifier(combination) ? [combination] : [];
+    }
 }
 
 /**
---
+++ algorithm/test-subjects.js
@@ -0,0 +1,257 @@
+var fisica = {
+    name: "Fisica",
+    commissions: [
+        {
+            commissionName: "A",
+            teachers: [
+                "Rinaldi"
+            ],
+            commissionTimes: [
+                {
+                    day: "Lunes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Miercoles",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Viernes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                }
+            ]
+        },
+        {
+            commissionName: "B",
+            teachers: [
+                "Medus"
+            ],
+            commissionTimes: [
+                {
+                    day: "Lunes",
+                    building: "Puerto Madero",
+                    hourFrom: "10.00",
+                    hourTo: "12.00"
+                },
+                {
+                    day: "Jueves",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Viernes",
+                    building: "Puerto Madero",
+                    hourFrom: "14.00",
+                    hourTo: "16.00"
+                }
+            ]
+        },
+        {
+            commissionName: "C",
+            teachers: [
+                "Dellelis"
+            ],
+            commissionTimes: [
+                {
+                    day: "Martes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Jueves",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Sabados",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                }
+            ]
+        }
+    ]
+};
+
+var matematica = {
+    name: "Matematica",
+    commissions: [
+        {
+            commissionName: "A",
+            teachers: [
+                "Rinaldi"
+            ],
+            commissionTimes: [
+                {
+                    day: "Lunes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Miercoles",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Viernes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                }
+            ]
+        },
+        {
+            commissionName: "B",
+            teachers: [
+                "Medus"
+            ],
+            commissionTimes: [
+                {
+                    day: "Lunes",
+                    building: "Puerto Madero",
+                    hourFrom: "10.00",
+                    hourTo: "12.00"
+                },
+                {
+                    day: "Jueves",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Viernes",
+                    building: "Puerto Madero",
+                    hourFrom: "14.00",
+                    hourTo: "16.00"
+                }
+            ]
+        },
+        {
+            commissionName: "C",
+            teachers: [
+                "Dellelis"
+            ],
+            commissionTimes: [
+                {
+                    day: "Martes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Jueves",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Sabados",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                }
+            ]
+        }
+    ]
+};
+
+var programacion = {
+    name: "Programacion",
+    commissions: [
+        {
+            commissionName: "A",
+            teachers: [
+                "Rinaldi"
+            ],
+            commissionTimes: [
+                {
+                    day: "Lunes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Miercoles",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Viernes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                }
+            ]
+        },
+        {
+            commissionName: "B",
+            teachers: [
+                "Medus"
+            ],
+            commissionTimes: [
+                {
+                    day: "Lunes",
+                    building: "Puerto Madero",
+                    hourFrom: "10.00",
+                    hourTo: "12.00"
+                },
+                {
+                    day: "Jueves",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Viernes",
+                    building: "Puerto Madero",
+                    hourFrom: "14.00",
+                    hourTo: "16.00"
+                }
+            ]
+        },
+        {
+            commissionName: "C",
+            teachers: [
+                "Dellelis"
+            ],
+            commissionTimes: [
+                {
+                    day: "Martes",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Jueves",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                },
+                {
+                    day: "Sabados",
+                    building: "Puerto Madero",
+                    hourFrom: "08.00",
+                    hourTo: "10.00"
+                }
+            ]
+        }
+    ]
+};
+
+var subjects = [
+    fisica,
+    matematica,
+    programacion
+];
